6156d7dba2555b8dd4b2c7e28441a5d5514c8714
minyryo <hieu.phamth@hcmut.edu.vn>
2024-12-08 23:54:15 +0700

    Update Project info

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,30 @@
 
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
+    .setTitle('WhatToOrder - AnGiHomNay API')
+    .setDescription(
+      `WhatToOrder - AnGiHomNay is a food delivery app focused on personalized restaurant and cuisine recommendations.`
+    )
+    .setVersion('1.0.1')
+    .setTermsOfService('https://whattoorder.com/terms/')
+    .setContact('Support', '', 'support@whattoorder.com')
+    .setLicense('Apache 2.0', 'http://www.apache.org/licenses/LICENSE-2.0.html')
+    .addServer('https://api.whattoorder.com/v1', 'Production API')
+    .addServer(
+      'https://app.swaggerhub.com/apis/HieuPhamThiHong/WhatToOrder/1.0.0',
+      'SwaggerHub API Auto Mocking'
+    )
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
